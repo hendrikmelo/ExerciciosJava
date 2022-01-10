a0e4d927432119bb303659abd4adfcbef1897673
Hendrik Melo <66493733+hendrikmelo@users.noreply.github.com>
2022-01-10 11:26:18 -0300

    testes metodo override

---
+++ curso java/override testes/src/program/program.java
@@ -0,0 +1,29 @@
+package program;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+import entities.SavingsAccount;
+
+public class program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Account acc1 = new Account(1001, "Alex", 1000.0);
+		acc1.withdraw(200.0);
+		System.out.println(acc1.getBalance());
+		
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
+		
+		sc.close();
+	}
+}
