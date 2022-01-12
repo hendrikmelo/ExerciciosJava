c83b15b841828b8f23c4ded3138142e5892ed14c
Hendrik Melo <66493733+hendrikmelo@users.noreply.github.com>
2022-01-12 14:52:47 -0300

    exercício reserva try catch
    Fazer um programa para ler os dados de uma reserva de hotel (número do quarto, data
    de entrada e data de saída) e mostrar os dados da reserva, inclusive sua duração em
    dias. Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
    novamente a reserva com os dados atualizados. O programa não deve aceitar dados
    inválidos para a reserva, conforme as seguintes regras:
    - Alterações de reserva só podem ocorrer para datas futuras
    - A data de saída deve ser maior que a data de entrada

---
+++ curso java/exericio reserva try catch/src/model/entities/Reservation.java
@@ -0,0 +1,70 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
+
+public class Reservation {
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkout;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) throws DomainException {
+		if (!checkout.after(checkin)) {
+			throw new DomainException("Check-out date must be after check-in date.");
+		}
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+	
+	public long duration() {
+		long diff = checkout.getTime() - checkin.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkin, Date checkout) throws DomainException {
+		Date now = new Date();
+		if (!checkout.before(checkin)) {
+			throw new DomainException("Reservation dates for update must be future dates.");
+		}
+		if (!checkout.after(checkin)) {
+			throw new DomainException("Check-out date must be after check-in date.");
+		}
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room " 
+				+ roomNumber 
+				+ ", checkin: " 
+				+ sdf.format(checkin) 
+				+ ", checkout: " 
+				+ sdf.format(checkout) 
+				+ ", " 
+				+ duration() +
+				" nighs";
+	}
+}
